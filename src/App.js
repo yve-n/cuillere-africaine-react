8643b26bcde6342d35a6162117f7b24e670a8aa1
yve-n <yve.nguetche@gmail.com>
2023-09-20 17:31:43 +0200

    code in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Menu from './Components/Menu/Menu';
+import Footer from './Components/Footer/Footer';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        // <>
+        //     <Router>
+        //         <Navbar />
+        //         <Routes>
+        //             <Route path="/" element={<Home />} />
+        //             <Route path="/about-us" element={<About />} />
+        //             <Route path="/menu" element={<Menu />} />
+        //             <Route path="/contact" element={<Contact/>} />
+        //         </Routes>
+        //         <Footer />
+        //     </Router>
+        // </>
+        <div>
+            <Navbar />
+            <div id="home">
+                <Home />
+            </div>
+            <div className="container">
+                <div id="about-us" className='row featurette'>
+                    <About />
+                </div>
+                <div id="menu" className='row featurette'>
+                    <Menu />
+                </div>
+                <div id="contact" className='row featurette'>
+                    <Contact />
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;

655eb4b94e4701a8c39c3b8c4c780847ca734d8f
yve-n <yve.nguetche@gmail.com>
2023-09-26 11:57:30 +0200

    improvement of the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (<>
-            <header className="d-flex flex-wrap justify-content-center fixed-top">
-                <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                    <span className="fs-4 text-black logo">CUILLÈRE AFRICAINE</span>
-                    {/* <img src={`${process.env.PUBLIC_URL}/images/logo_africaine.png`} alt="logo" className="bd-placeholder-img card-img-top" width="50%" height="50" aria-label="Placeholder: Thumbnail" /> */}
-                </a>
+            <header className="d-flex flex-wrap justify-content-center fixed-top bg-dark">
+                <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                    <img src={`${process.env.PUBLIC_URL}/images/logo_africaine.png`} alt="logo" className="bd-placeholder-img card-img-top" width="60%" height="60" aria-label="Placeholder: Thumbnail" />
+                </Link>
 
                 <ul className="navbar nav">
                     <li className="nav-item ">
-                        <a className="nav-link active onglet" href="#home" aria-current="page">ACCUEIL</a>
+                        <Link className="nav-link active onglet" to="/" aria-current="page">ACCUEIL</Link>
 
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link onglet" href="#about-us">À PROPOS</a>
+                        <Link className="nav-link onglet" to="/about-us">À PROPOS</Link>
 
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link onglet" href="#menu">MENU</a>
+                        <Link className="nav-link onglet" to="/menu">MENU</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link onglet" href="#contact">CONTACT</a>
+                        <Link className="nav-link onglet" to="/contact">CONTACT</Link>
                     </li>
                 </ul>
             </header>

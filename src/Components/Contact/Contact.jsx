0bb50dc478cfc570c50b2b7ccfa53533825141c6
yve-n <yve.nguetche@gmail.com>
2023-09-28 11:13:29 +0200

    add content for this page including a map

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,39 +1,44 @@
 import React from 'react'
 
 export default function Contact() {
+    const api_key = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+
     return (
         <>
-            <hr className="featurette-divider"></hr>
-            <div class="bg-dark text-secondary px-4 py-5 text-center">
-                <div class="py-5">
-                    <h1 class="display-5 fw-bold text-white">Dark color hero</h1>
-                    <div class="col-lg-6 mx-auto">
-                        <p class="fs-5 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                            <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
-                            <button type="button" class="btn btn-outline-light btn-lg px-4">Secondary</button>
+            <section className="py-3 text-center container">
+                <div className="row py-lg-3">
+                    <div className="col-lg-6 col-md-8 mx-auto">
+                        <h1 className="fw-light">Contactez-nous</h1>
+                        <p className="lead text-body-secondary">
+                            Pour toute question ou réservation, n'hésitez pas à nous contacter.
+                            Nous sommes situés au cœur de Paris et sommes toujours heureux de vous accueillir.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <div className="container marketing">
+                <div className="row featurette d-sm-flex justify-content-sm-center">
+                    <div className="col-lg-6 col-md-6 col-sm-auto pb-sm-2">
+                        <div className="card shadow-sm card-n">
+                            <h5 className="card-title"><i className="bi bi-shop"></i> Cuillère Africaine</h5>
+                            <p className="card-text"><i className="bi bi-geo-alt-fill"></i> 36 Rue Mathis 75019 Paris</p>
+                            <p className="card-text"><i className="bi bi-telephone-fill"></i> 06.60.26.60.34</p>
                         </div>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-auto">
+                        <iframe
+                            title="la Carte Google Maps du restaurant"
+                            width="100%"
+                            height="100%"
+                            frameborder="0" style={{ border: 0 }}
+                            referrerpolicy="no-referrer-when-downgrade"
+                            src={`https://www.google.com/maps/embed/v1/place?key=${api_key}&q=Cuillère+Africaine,Paris+France&center=48.891386426126225,2.373363262240684&zoom=18&maptype=satellite`}
+                            allowfullscreen>
+                        </iframe>
                     </div>
                 </div>
             </div>
-            <hr className="featurette-divider"></hr>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt rutrum diam commodo lacinia. Duis at turpis id ante ullamcorper posuere ut id leo. Quisque semper orci suscipit nisi dignissim hendrerit. Vestibulum gravida diam lectus, nec suscipit ex bibendum id. Etiam sodales leo vel placerat tempus. Praesent ligula nunc, aliquet vitae mauris quis, suscipit ultricies nunc. Sed eu sapien a sapien maximus scelerisque eu nec augue. Praesent suscipit neque sit amet ipsum convallis suscipit. Integer sed lectus felis. Aliquam quis urna in est mollis consequat eu eget libero. Donec tincidunt orci vel libero vehicula luctus in at nulla. Phasellus nec augue ipsum. Nulla diam metus, blandit nec pharetra at, varius vel velit. Phasellus gravida gravida erat, ut aliquet urna tempus non. Phasellus congue, nulla quis rutrum ultrices, orci neque feugiat lacus, a cursus nunc urna quis nisi. Nullam mauris tortor, consectetur non dignissim vel, dignissim id velit.
 
-                Phasellus a diam tincidunt, feugiat nisi sed, ultrices ante. Phasellus dignissim dignissim velit, ac pellentesque tortor cursus ac. Nunc quis feugiat ante. Nullam porta mauris dolor, eget bibendum libero lobortis a. Nulla augue augue, pretium ut bibendum sed, sodales eu ligula. Maecenas nulla erat, consectetur et fermentum non, blandit a augue. Nam et tempor felis. Integer aliquam velit quis mi interdum sagittis et quis erat. Aliquam molestie, felis vitae pulvinar tincidunt, erat enim hendrerit felis, non dignissim erat arcu non tellus. Sed nec purus non est maximus elementum eget eget turpis. Phasellus pellentesque purus ut felis ornare interdum. In pretium tincidunt orci, eu dictum erat posuere sit amet. Integer pellentesque, arcu at facilisis sagittis, sapien felis placerat enim, a convallis leo ex vel tortor.
-
-                Quisque metus tellus, mattis quis dapibus vitae, maximus eget massa. Donec dapibus ante velit, venenatis efficitur nunc mattis in. Sed viverra ligula egestas dui lacinia, eu vulputate ligula efficitur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla eget sapien accumsan, fermentum lectus ut, pellentesque odio. Nulla turpis lectus, ultricies et aliquam vel, blandit id massa. Sed fermentum quam vel hendrerit pharetra. Mauris vitae hendrerit massa. Quisque justo lectus, lobortis non dui in, lacinia varius nulla. Phasellus rhoncus nisl neque, ut pharetra turpis egestas sit amet. Nulla eget libero eget eros congue ullamcorper.
-
-                Fusce blandit, libero sit amet elementum venenatis, turpis massa convallis velit, id auctor dui tortor quis nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum et vulputate erat. Quisque porttitor non massa eu viverra. Aliquam lobortis pharetra eros, sit amet dignissim mi hendrerit eget. Nullam at nulla quis quam scelerisque cursus eget et tortor. Nunc ultricies a metus a scelerisque. Sed ac mi vitae justo dictum sodales. In tristique ligula neque, at hendrerit eros semper vel. Suspendisse eu sem metus. Praesent ut justo at lorem interdum efficitur. Donec in rhoncus justo, ut interdum dui.
-
-                Etiam mollis sapien at libero viverra, tempus malesuada tellus vehicula. Etiam non neque vitae mi eleifend sollicitudin eu ut urna. Nulla facilisi. Etiam feugiat cursus augue. Etiam nisl leo, blandit vel imperdiet ut, tincidunt in leo. Pellentesque eu lacus justo. Praesent turpis sapien, auctor quis justo quis, lobortis mollis mauris. Morbi sit amet placerat nibh, id interdum diam. Maecenas euismod faucibus enim vitae imperdiet. Cras tincidunt metus a vehicula lobortis. Sed ut ultricies odio. Nullam eu est et ex euismod mattis eget porttitor sapien. Praesent et rhoncus quam, sit amet congue mi. Proin nisi nulla, ultricies vel justo placerat, vulputate elementum erat.
-
-                Vestibulum a velit id lectus fermentum hendrerit. Nullam blandit fermentum diam, eu bibendum erat eleifend in. Nam fermentum pellentesque urna, auctor vestibulum nunc gravida sed. Morbi vel purus fringilla sem posuere consectetur at et nisl. Nulla mattis suscipit massa, ut euismod nulla finibus a. Pellentesque rhoncus lacus eget neque molestie, at rutrum neque iaculis. Praesent et massa sit amet nunc porta sagittis. Donec a tempor enim, nec pretium urna. Integer lacinia enim ut ultrices vestibulum. Aenean mollis arcu ut odio dictum tincidunt. In auctor finibus dolor, id cursus leo porta sed. Nam vitae tempus nulla. Praesent pellentesque tellus eget dui pellentesque, vel rutrum sem semper. Proin ultrices nisl id iaculis suscipit.
-
-                Vestibulum vestibulum mauris eget mattis tempor. Praesent dictum, turpis vitae tristique rhoncus, risus diam ullamcorper purus, sed imperdiet odio erat eget elit. Phasellus accumsan tincidunt metus mattis egestas. Pellentesque quam neque, molestie ut venenatis quis, ultricies in lectus. Vivamus et fermentum augue, quis viverra augue. Fusce in tempor risus. Maecenas aliquet vestibulum aliquet. Donec dolor ex, aliquam ut libero vel, convallis varius urna. Nam quis nunc tortor. Morbi at odio eget odio consectetur accumsan. Donec auctor erat sed consequat sodales.
-
-                Nunc tristique luctus enim. Sed malesuada magna a odio pharetra suscipit. Aliquam erat volutpat. Mauris nunc justo, pharetra laoreet arcu sed, condimentum varius mauris. Sed semper porttitor tellus, quis sollicitudin augue sollicitudin sit amet. Integer sit amet lacinia ex, sit amet eleifend leo. Sed finibus nisi sed enim rhoncus, elementum tempor libero porta. Ut mi tortor, sagittis ac sem eget, pretium suscipit mi. Proin varius vel libero non finibus. Etiam tincidunt lorem arcu, sed fringilla est pretium vitae. Nam pharetra purus sit amet fringilla bibendum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam a pellentesque justo, sit amet lacinia arcu. Vestibulum fermentum arcu dapibus magna vestibulum lacinia. Donec aliquam in magna ac egestas. Morbi sit amet tempor augue, id placerat nibh.
-            </p>
         </>
     )
 }

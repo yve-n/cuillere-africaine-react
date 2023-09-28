7febdae0973345ba14c5f3c5f954e5a7920314e5
yve-n <yve.nguetche@gmail.com>
2023-09-28 11:24:09 +0200

    improve accessibility by adding aria-label description. change the logo.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,24 +4,24 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (<>
             <header className="d-flex flex-wrap justify-content-center fixed-top bg-dark">
-                <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                    <img src={`${process.env.PUBLIC_URL}/images/logo_africaine.png`} alt="logo" className="bd-placeholder-img card-img-top" width="60%" height="60" aria-label="Placeholder: Thumbnail" />
+                <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none" aria-label="logo" >
+                    <img src={`${process.env.PUBLIC_URL}/logo_ca_192.png`} alt="logo" className="card-img-top" width="60%" height="60" />
                 </Link>
 
                 <ul className="navbar nav">
                     <li className="nav-item ">
-                        <Link className="nav-link active onglet" to="/" aria-current="page">ACCUEIL</Link>
+                        <Link className="nav-link active onglet" to="/" aria-current="page" aria-label="Accueil">ACCUEIL</Link>
 
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link onglet" to="/about-us">À PROPOS</Link>
+                        <Link className="nav-link onglet" to="/about-us" aria-label="A propos">À PROPOS</Link>
 
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link onglet" to="/menu">MENU</Link>
+                        <Link className="nav-link onglet" to="/menu" aria-label="Menu">MENU</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link onglet" to="/contact">CONTACT</Link>
+                        <Link className="nav-link onglet" to="/contact" aria-label="Contact">CONTACT</Link>
                     </li>
                 </ul>
             </header>
